ce085911b2b215d3fda8e4f970716275fd879eaa
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-03-08 05:37:41 +0100

    on tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { logo, menu, close } from "../assets";
+import { styles } from "../style";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Asura <span className="sm:block hidden">| Chandrasura</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
